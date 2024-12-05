6db4571cf6bf8b7daa7148de98dbe323c3fd348b
SERENA FERRARO <serenaferraro21@gmail.com>
2024-12-05 22:11:34 +0100

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import MyNavbar from './components/MyNavbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-     <h1>Testo di prova</h1>
+  
+    <div className="App"> 
+     <header><MyNavbar /></header>
+     <main></main>
     </div>
   );
 }
