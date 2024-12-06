4fcdf3c28d077c54b61e4727bbf87eeb00167908
SERENA FERRARO <serenaferraro21@gmail.com>
2024-12-06 09:38:38 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import MyFooter from './components/MyFooter';
 import MyNavbar from './components/MyNavbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function App() {
   return (
   
-    <div className="App"> 
-     <header><MyNavbar /></header>
-     <main></main>
+    <div className="App d-flex flex-column vh-100"> 
+    <MyNavbar />
+    <main className='flex-grow-1'></main>
+    <MyFooter/>
     </div>
   );
 }
---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,24 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Envelope, Github } from 'react-bootstrap-icons';
+const MyFooter = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    return (
+        <footer>
+            <Row>
+                <Col xs={12}>
+                    <div className=' bg-dark text-white text-center py-2'>
+                        <Envelope className='me-3'></Envelope>
+                            <Github className='me-3'></Github>
+                      <p className='m-3'>Copyright © {year}</p>
+                    </div>
+                   
+                  
+                </Col>
+            </Row>
+        </footer>
+    )
+}
+
+export default MyFooter;
